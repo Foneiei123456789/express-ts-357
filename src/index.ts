20daf1c9386e4e6ce09f4ebad32b971f4058f5ba
357Phatcharaphorn <fon291147@gmail.com>
2024-09-11 13:38:32 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+import routes from './routes';
+//...
+// applying the routes to the basepath '/api'
+app.use('/api', routes);
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+})
